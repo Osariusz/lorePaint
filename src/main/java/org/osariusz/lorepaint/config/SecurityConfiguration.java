a1a85b54e78662d0fd5e3c600dee92476cf32e02
Osariusz <osariusz@interia.pl>
2024-03-27 18:15:49 +0100

    Login and JWT start

--- src/main/java/org/osariusz/lorepaint/config/SecurityConfiguration.java
+++ src/main/java/org/osariusz/lorepaint/config/SecurityConfiguration.java
@@ -5,32 +5,58 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration {
 
+    private JwtAuthEntryPoint authEntryPoint;
+
     private CustomUserDetailsService customUserDetailsService;
 
     @Autowired
-    public SecurityConfiguration(CustomUserDetailsService customUserDetailsService) {
+    public SecurityConfiguration(CustomUserDetailsService customUserDetailsService){//, JwtAuthEntryPoint authEntryPoint) {
         this.customUserDetailsService = customUserDetailsService;
+        //this.authEntryPoint = authEntryPoint;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authz -> authz.requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated());
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+                    configuration.setAllowedMethods(List.of("*"));
+                    configuration.setAllowedHeaders(List.of("*"));
+                    return configuration;
+                }))
+                //.formLogin(Customizer.withDefaults())
+                .exceptionHandling(exception -> {exception.authenticationEntryPoint(authEntryPoint).accessDeniedPage("/error/access-denied");})
+
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //.authenticationProvider(authenticationProvider()).addFilterBefore()
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/auth/**","/login*").permitAll().requestMatchers("/error").authenticated().anyRequest().denyAll());
 
         return http.build();
     }
+
+
 
     @Bean
     public AuthenticationManager authenticationManager(
