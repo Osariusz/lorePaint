8a56ee668d31aca7ff3cde98eaa859c00f62ebff
Osariusz <osariusz@interia.pl>
2024-05-25 21:22:56 +0200

    Add GM as a Member as well

--- src/main/java/org/osariusz/lorepaint/lore/LoreService.java
+++ src/main/java/org/osariusz/lorepaint/lore/LoreService.java
@@ -58,9 +58,11 @@ public class LoreService {
         Lore lore = modelMapper.map(loreCreateDTO, Lore.class);
         lore.setPlaces(new ArrayList<>());
         lore.setMap(mapService.createMap());
-        LoreUserRole loreUserRole = loreUserRoleService.assignLoreUserRole(creator, lore, RoleNames.LORE_GM_ROLE_NAME);
+        LoreUserRole loreGMRole = loreUserRoleService.assignLoreUserRole(creator, lore, RoleNames.LORE_GM_ROLE_NAME);
+        LoreUserRole loreMemberRole = loreUserRoleService.assignLoreUserRole(creator, lore, RoleNames.LORE_MEMBER_ROLE_NAME);
         saveLore(lore);
-        loreUserRoleService.saveRole(loreUserRole);
+        loreUserRoleService.saveRole(loreGMRole);
+        loreUserRoleService.saveRole(loreMemberRole);
         return lore;
     }
 
